c13afeace021ccb9b618699fe765f0ea1d185e80
Lightening Gibson <lighteningibson@gmail.com>
2022-02-07 13:55:36 -0600

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution here
+
+
+function findMatching(drivers, name) {
+
+ 
+   return drivers.filter(driver => driver.toUpperCase() === name.toUpperCase())
+}
+
+
+
+
+function fuzzyMatch(drivers, name) {
+
+        let length = name.length
+    return drivers.filter(driver => driver.slice(0, length) === name)
+
+
+}
+
+function matchName(drivers, personName) {
+
+    return drivers.filter(driver => driver.name === personName)
+
+
+}
